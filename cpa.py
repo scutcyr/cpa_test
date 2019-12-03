1ab91f905b66ceeee8c0b151830433a1c097884a
Yirong Chen <1132461715@qq.com>
2019-12-03 17:23:38 +0800

    Add files via upload

---
+++ cpa.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 中国人口分析
+# **作者**：陈艺荣   
+# **依赖**：python3.7、pytorch1.3.0    
+# **参考**:https://morvanzhou.github.io/tutorials/
+
+# 导入相应包
+import torch
+import torch.nn.functional as F
+
+
+class PAnet(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output, hidden_num=1):
+        super(PAnet, self).__init__()
+        self.hidden1 = torch.nn.Linear(n_feature, n_hidden)   # hidden1 layer
+        self.hidden2 = torch.nn.ModuleList([torch.nn.Linear(n_hidden, n_hidden) for i in range(hidden_num)])
+        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer        
+        
+    def forward(self, x):
+        x = F.relu(self.hidden1(x)) 
+        for i, h in enumerate(self.hidden2):
+            x = F.relu(h(x))
+        x = self.predict(x) # linear output
+        return x
+
+class OldPAnet(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(OldPAnet, self).__init__()
+        self.hidden1 = torch.nn.Linear(n_feature, n_hidden)   # hidden1 layer
+        self.hidden2 = torch.nn.Linear(n_hidden, n_hidden)   # hidden2 layer
+        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer        
+        
+    def forward(self, x):
+        x = F.relu(self.hidden1(x)) 
+        x = F.relu(self.hidden2(x)) 
+        x = F.relu(self.hidden2(x)) 
+        x = F.relu(self.hidden2(x)) 
+        x = self.predict(x) # linear output
+        return x
